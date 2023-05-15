1ca70473fba134b1c27b739a8b613c27ffeec9e9
derMicha <dermicha@trackle.de>
2023-05-15 19:14:33 +0200

    fixed scripts

--- keycloak/scripts/export-keycloak-data.sh
+++ keycloak/scripts/export-keycloak-data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker exec -it bin-da_kc /opt/keycloak/bin/kc.sh export --dir /tmp/export
-docker cp bin-da_kc:/tmp/export ./keycloak/data
+docker exec -it binda_kc /opt/keycloak/bin/kc.sh export --dir /tmp/export
+docker cp binda_kc:/tmp/export ./keycloak/data
