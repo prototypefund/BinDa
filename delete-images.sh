bd8fde65ec3740c4c41beb9ce3d3120c8eecd14b
derMicha <dermicha@trackle.de>
2023-08-04 12:23:56 +0200

    fix: now you can select image to be deleted usage: ./delete-images.sh {API|UI}

--- delete-images.sh
+++ delete-images.sh
@@ -1,13 +1,31 @@
-#!/bin/bash
+#!/bin/bash -x
 
 DOCKER=$(which docker)
 
-UIID=$(${DOCKER} images |grep binda_ui|awk '{print $3}')
-if [[ ! -z "${UIID}" ]]; then
-  docker rmi "${UIID}"
-fi
+function delete_image () {
+  IMAGE_KEY=$1
+  if [[ ! -z "${IMAGE_KEY}" ]]; then
+    if [[ "${IMAGE_KEY}" == "API" ]]; then
+      IMAGE_NAME="binda_ui"
+    elif [[ "${IMAGE_KEY}" == "UI" ]]; then
+      IMAGE_NAME="binda_api"
+    fi
 
-APIID=$(${DOCKER} images |grep binda_api|awk '{print $3}')
-if [[ ! -z "${APIID}" ]]; then
-  docker rmi "${APIID}"
+    if [[ ! -z "${IMAGE_NAME}" ]]; then
+      IMAGE_ID=$(${DOCKER} images |grep "${IMAGE_NAME}"|awk '{print $3}')
+      if [[ ! -z "${IMAGE_ID}" ]]; then
+        docker rmi "${IMAGE_ID}"
+      fi
+    else
+      echo "invalid image key, not 'API' or 'UI'"
+    fi
+  fi
+}
+
+KEY=$1
+
+if [[ ! -z "${KEY}" ]]; then
+  delete_image "${KEY}"
+else
+  echo "usage: $0 {API|UI}"
 fi
